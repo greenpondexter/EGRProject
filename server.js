8f7b4cc6b6fdacf877f5918707e7a021c1d781f4
greenpondexter <greenpondexter@gmail.com>
2015-03-27 15:26:53 -0700

    Restructured so index was being called from hot

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })**/
 
-app.use(express.static(__dirname+'/client/build'));
+app.use(express.static(__dirname));
 
 
 var server = app.listen(3000, function () {
